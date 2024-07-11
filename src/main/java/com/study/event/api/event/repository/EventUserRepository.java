736a5e5cb81ffe319b85601a771b22f7a2bb5429
hyerin11 <hodolin24@naver.com>
2024-07-11 09:55:04 +0900

    # 인증코드 인증하기_1

--- src/main/java/com/study/event/api/event/repository/EventUserRepository.java
+++ src/main/java/com/study/event/api/event/repository/EventUserRepository.java
@@ -1,10 +1,16 @@
 package com.study.event.api.event.repository;
 
+import com.study.event.api.event.entity.Event;
 import com.study.event.api.event.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface EventUserRepository extends JpaRepository<EventUser, String> {
 
     // query method로 Jpql 생성
     boolean existsByEmail(String email);
+
+    //
+    Optional<EventUser> findByEmail(String email);
 }
